10c8b725019e909bd3b33e5c71e6e3b451164a71
JimzSnowby <117113270+JimzSnowby@users.noreply.github.com>
2025-04-01 12:46:28 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {    // Entry point
+        int[] cars = {3, -5, 4, -2, -1};
+    }
+
+    public int[] CarCrash(int[] cars)
+    {
+        int[] result = new int[10];
+
+        return result;
+    }
+}
