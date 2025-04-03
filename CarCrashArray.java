98cc4895085ba4eb789fed69b4fa0ff596121c89
JimzSnowby <117113270+JimzSnowby@users.noreply.github.com>
2025-04-03 17:57:34 +0100

    Refactored and renamed

--- CarCrashArray.java
+++ CarCrashArray.java
@@ -1,7 +1,29 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Main {
+/*Car Array Problem
+
+There is a one lane road going West to East.
+On this road are cars (represented by an array). Each cars speed can be different (represented by the absolute int value) and can be going in different directions (represented by + or -), for example:
+If the array element is 5, the car is going East at a speed of 5.
+If the array element is -3, the car is going West at a speed of 3.
+
+With the road only being 1 lane, these cars will crash. Write a function that will output the remaining cars.
+
+Rules:
+1. The slower of two cars (elements) is removed from the lane (array).
+2. If the cars are moving the same speed in the opposite direction, both are removed.
+3. Cars moving the same direction do not interact with one another, regardless of speed.
+4. Cars with a value of 0 do not interact with any other car.
+5. Do not change return types of functions or classes.
+
+Example Input:
+cars [3, -3, 2, -1, 1, 5, -5]
+Example Output:
+[2, 1]
+ */
+
+public class CarCrashArray {
     public static void main(String[] args) {
         int[] cars1 = {3, -3, 2, -1, 1, 5, -5};
         int[] cars2 = {3, -5, 4, -2, -1};
